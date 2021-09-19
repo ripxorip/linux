b200ee648a068247ca2ccd8ad486a909c309744c
Philip K. Gisslow <ripxorip@gmail.com>
2021-09-19 19:34:05 +0200

    Adds some helpers used to deploy to my pinephone

---
+++ scripts/install_kernel_pinephone.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp -rf arch/arm64/boot/dts /home/ripxorip/dev/pine/arch/tmp/
+cp -rf scripts/extlinux /home/ripxorip/dev/pine/arch/tmp/
+cp builds/arch/arm64/boot/Image /home/ripxorip/dev/pine/arch/tmp/
